356dac9fc3b0a83f6aa5031a75bd6ab8dd9749e0
huangbin <575395982@qq.com>
2018-11-23 05:08:58 +0800

    ADD 添加自定义组件：orderInfoItemText 用于显示订单条目

---
+++ components/orderInfoItemText/orderInfoItemText.js
@@ -0,0 +1,79 @@
+// components/orderInfoItemText.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        t1: {
+            type: String,
+            value: "title1"
+        },
+
+        t2: {
+            type: String,
+            value: "title2"
+        },
+        t3: {
+            type: String,
+            value: "title3"
+        },
+        t4: {
+            type: String,
+            value: "title4"
+        },
+        t5: {
+            type: String,
+            value: "title5"
+        },
+        t6: {
+            type: String,
+            value: "title6"
+        },
+        t7: {
+            type: String,
+            value: "title7"
+        },
+        t8: {
+            type: String,
+            value: "title8"
+        },
+        t9: {
+            type: String,
+            value: "title9"
+        },
+        t10: {
+            type: String,
+            value: "title10"
+        },
+        t11: {
+            type: String,
+            value: "title11"
+        },
+        t12: {
+            type: String,
+            value: "title12"
+        },
+        t13: {
+            type: String,
+            value: "title13"
+        },
+        t14: {
+            type: String,
+            value: "title14"
+        },
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
